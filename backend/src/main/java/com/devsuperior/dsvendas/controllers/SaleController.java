4e7494b48232aa466c64b605503850ff3939ea03
bruno-almeida-silva <balmeida80@gmail.com>
2021-05-05 19:47:55 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -2,6 +2,8 @@ package com.devsuperior.dsvendas.controllers;
 
 import com.devsuperior.dsvendas.Services.SaleService;
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("sales")
@@ -23,4 +27,13 @@ public class SaleController {
         return new ResponseEntity(service.findAll(pageable), HttpStatus.OK);
     }
 
+    @GetMapping("/amount-by-seller")
+    public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+        return new ResponseEntity(service.amountGroupedBySeller(), HttpStatus.OK);
+    }
+
+    @GetMapping("/success-by-seller")
+    public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+        return new ResponseEntity(service.successGroupedBySeller(), HttpStatus.OK);
+    }
 }
